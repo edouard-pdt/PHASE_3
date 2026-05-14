d6fdb9415f01c89f49bf900a0d5e8ae66f3b95ee
edouard-pdt <e.poudat@lecolededesign.com>
2026-05-14 16:53:37 +0200

    Initialize main.tsx with basic React structure

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Mon site fonctionne ! 🎉</h1>
+      <p>Le fichier main.tsx a bien été trouvé.</p>
+    </div>
+  </React.StrictMode>,
+)
