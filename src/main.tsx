6afa323baff25063b3670bb014da115fa6127e3e
edouard-pdt <e.poudat@lecolededesign.com>
2026-05-14 17:15:45 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1>Mon site fonctionne ! 🎉</h1>
-      <p>Le fichier main.tsx a bien été trouvé.</p>
-    </div>
-  </React.StrictMode>,
-)
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
