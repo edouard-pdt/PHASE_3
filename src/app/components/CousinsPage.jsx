b74bc19c3eba1f81579ed2aecaa3bdf3feddfac0
edouard-pdt <e.poudat@lecolededesign.com>
2026-05-17 18:03:59 +0200

    Update CousinsPage.jsx

--- src/app/components/CousinsPage.jsx
+++ src/app/components/CousinsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
-import Header from "./Header";
+import { Header } from './Header';
 
 const colors = {
   yellow: "#F6C453",
